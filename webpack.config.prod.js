b8036b3e611c8e2593204be5e6f3f260c78bf587
Bmogul <98663272+Bmogul@users.noreply.github.com>
2023-03-27 23:49:03 -0400

    edited config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'production',
-  entry: './src/js/index.js',
+  entry: './src/index.js',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, './docs'),
